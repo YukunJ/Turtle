749d5ced244f49dae11451eb7d60c7035f6aa123
YukunJ <yukunj@andrew.cmu.edu>
2023-01-06 12:47:23 -0500

    Cache layer: optimize file transfer. reduce copying buffer

--- src/http/response.cpp
+++ src/http/response.cpp
@@ -66,13 +66,6 @@ void Response::Serialize(std::vector<unsigned char>& buffer) {  // NOLINT
   str_stream << CRLF;
   std::string response_head = str_stream.str();
   buffer.insert(buffer.end(), response_head.begin(), response_head.end());
-  if (resource_url_.has_value() && should_transfer_content_) {
-    LoadFile(resource_url_.value(), buffer);
-  }
-}
-
-void Response::SetShouldTransferContent(bool should_transfer_content) {
-  should_transfer_content_ = should_transfer_content;
 }
 
 }  // namespace TURTLE_SERVER::HTTP
